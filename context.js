7c19d2d81ec181fa22723d946cb5686b2b24f61d
Anh Hao <hi@anhhao.me>
2021-02-20 21:08:09 +0700

    Add URL and hash

--- context.js
+++ context.js
@@ -2,6 +2,8 @@ const vm = require('vm');
 const logger = require('./logger');
 const cron = require('node-cron');
 const unirest = require('unirest');
+const { URL } = require('url');
+const crypto = require('crypto');
 
 module.exports = name => {
   const context = vm.createContext({
@@ -16,9 +18,25 @@ module.exports = name => {
       }));
     },
     log(msg){
+      if (msg instanceof Error){
+        logger.error(`${name} - ${msg.message}`);
+        return;
+      }
+
+      if (typeof msg === 'object'){
+        logger.info(`${name} - ${JSON.stringify(msg, 0, 2)}`);
+        return;  
+      }
+
       logger.info(`${name} - ${msg}`);
     },
-    get: unirest.get
+    get: unirest.get,
+    URL,
+    hash(text){
+      const al = crypto.createHash('sha256');
+      al.update(text);
+      return al.digest('hex');
+    }
   });
 
   return context;
